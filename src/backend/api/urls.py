eb3929a8c79fc7eff11f11b1036289d12f23f588
mostafa2k <m2kappswindows8.1@gmail.com>
2025-03-31 18:56:10 +0330

    feat(api): add URL patterns for NoteListCreate and NoteDelete views

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -5,3 +5,8 @@ from . import views
 urlpatterns = [
     path("user/register/", views.CreateUserView.as_view(), name="register"),
 ]
+
+urlpatterns += [
+    path("note/", views.NoteListCreateView.as_view(), name="note-list-create"),
+    path("note/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+]
