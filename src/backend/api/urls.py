0f9a8a4363c8b1e2daf8b8b80b6991c6b76beddd
mostafa2k <m2kappswindows8.1@gmail.com>
2025-04-02 09:54:58 +0330

    feat(api): update note URLs to use plural form for consistency

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path("note/", views.NoteListCreateView.as_view(), name="note-list-create"),
-    path("note/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+    path("notes/", views.NoteListCreateView.as_view(), name="note-list-create"),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 ]
